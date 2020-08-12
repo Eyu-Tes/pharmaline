d426d094ba0e2a5a107e7bb889b931fff580cc2c
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-08-12 11:04:52 +0300

    added store, checkout and thankyou pages; mapped appropriate urls

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import index, cart, about
+from .views import index, cart, about, store, checkout, thankyou
 
 
 app_name = 'store'
 
 urlpatterns = [
     path('', index, name='home'),
+    path('store/', store, name='store'),
     path('cart/', cart, name='cart'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('checkout/', checkout, name='checkout'),
+    path('thankyou/', thankyou, name='thankyou')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from medication.models import Medication
 
 
 def index(request):
@@ -11,3 +12,19 @@ def cart(request):
 
 def about(request):
     return render(request, 'store/about.html')
+
+
+def store(request):
+    try:
+        meds = Medication.objects.all()[:12]
+    except Medication.DoesNotExist:
+        meds = None
+    return render(request, 'store/store.html', context={'meds': meds})
+
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
+
+
+def thankyou(request):
+    return render(request, 'store/thankyou.html')
