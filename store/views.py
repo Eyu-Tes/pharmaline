187b68c0584686425ece4cca91b96cc587742aa7
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-08-17 09:34:41 -0600

    wip/cart (#8)
    * added the Cart and CartItem models
    
    * renamed a field in the 'Cart' object
    
    * adding items to cart complete; user_session cookie added for cart management
    
    * renamed the cart and drug attributes in CartItem
    
    * renamed forgotten Cart and CartItem attributes used in store/views.py
    
    * added a 'quantity' attribute to the CartItem model
    
    * added a 'total_price' attribute to the CartItem model
    
    * changed the variables used in store/cart.html to reflect the changes made to the CartItem model
    
    * used the 'quantity' model attribute in store/views.py
    
    * logic to delete items from the cart written; duplicate items only affect quantity

--- store/views.py
+++ store/views.py
@@ -1,36 +1,127 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from .models import Medication
+
+from .models import Medication, Cart, CartItem
+from django.utils.timezone import timezone
+
+from datetime import datetime
+import re
+
+import shortuuid
+
+
+def get_cart_count(cart):
+    cart_items = CartItem.objects.filter(cart=cart)
+    cart_count = sum([cart_item.quantity for cart_item in cart_items])
+    return cart_count
+
+
+def set_user_session_cookie(request, response):
+    response.set_cookie('user_session', get_user_session_cookie(request))
+    return response
+
+
+def get_user_session_cookie(request):
+    try:
+        user_session = request.COOKIES['user_session']
+    except KeyError:
+        user_session = shortuuid.random(length=30)
+
+    return user_session
 
 
 def index(request):
-    return render(request, 'store/index.html')
+    cart_count = get_cart_count(get_cart(request))
+    response = render(request, 'store/index.html', context={'cart_count': cart_count})
+    return set_user_session_cookie(request, response)
 
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    cart = get_cart(request)
+    # TODO: add or remove the requested medication from the cart
+    cart_items = CartItem.objects.filter(cart=cart)
+
+    if request.method == 'POST':
+        if 'remove_med' in request.POST:
+            med = Medication.objects.get(id=request.POST['remove_med'])
+            cart_items.filter(drug=med).delete()
+        else:
+            add_med_to_cart(request, cart_items)
+            # Adding a new medication doesn't necessarily mean you want to checkout.
+            # Therefore, the response will take the user to the 'store' page
+            return store(request)
+
+    # TODO: calculate proper subtotals and totals (dlivery fee, vat, etc...)
+    cart_count = get_cart_count(cart)
+    total = sum([cart_item.total_price for cart_item in cart_items])
+    response = render(request, 'store/cart.html',
+                      context={'cart_count': cart_count, 'cart_items': cart_items, 'subtotal': total, 'total': total})
+    return set_user_session_cookie(request, response)
+
+
+def add_med_to_cart(request, cart_items):
+    med = Medication.objects.get(id=request.POST['med_id'])
+    med_quantity = re.sub('[^\\d]', '', request.POST['quantity'])
+    if re.match('^0+$', med_quantity) or (med_quantity == ''):
+        med_quantity = 1
+    med_quantity = int(med_quantity)
+    # If the medication is already in the cart increase its quantity by `med_quantity`
+    if cart_items.filter(drug=med).first():
+        cart_item = cart_items.get(drug=med)
+        cart_item.quantity += med_quantity
+        cart_item.total_price += (
+                med.price * cart_item.quantity)
+        cart_item.save()
+    else:  # The medication added to the cart must be inserted to the cart_item table
+        new_item = CartItem(cart=get_cart(request), drug=med, quantity=med_quantity)
+        new_item.total_price = med.price * new_item.quantity
+        new_item.save()
 
 
 def about(request):
-    return render(request, 'store/about.html')
+    cart_count = get_cart_count(get_cart(request))
+    response = render(request, 'store/about.html', context={'cart_count': cart_count})
+    return set_user_session_cookie(request, response)
 
 
 def store(request):
+    cart_count = get_cart_count(get_cart(request))
     try:
         meds = Medication.objects.all()[:12]
     except Medication.DoesNotExist:
         meds = None
-    return render(request, 'store/store.html', context={'meds': meds})
+    response = render(request, 'store/store.html', {'meds': meds, 'cart_count': cart_count})
+    return set_user_session_cookie(request, response)
 
 
 def details(request, med_id):
-    med = get_object_or_404(Medication, pk=med_id)
-    return render(request, 'store/details.html', context={'med': med})
+    cart_count = get_cart_count(get_cart(request))
+    response = render(request, 'store/details.html',
+                      context={'med': get_object_or_404(Medication, pk=med_id),
+                               'cart_count': cart_count})
+    return set_user_session_cookie(request, response)
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html')
+    cart_count = get_cart_count(get_cart(request))
+    response = render(request, 'store/checkout.html', {'cart_count': cart_count})
+    return response
 
 
 def thankyou(request):
-    return render(request, 'store/thankyou.html')
+    response = render(request, 'store/thankyou.html')
+    return response
+
+
+def get_cart(request):
+    user_session = get_user_session_cookie(request)
+    try:
+        # Try to get a cart registered associated with `user_session`
+        cart = Cart.objects.get(user_session=user_session)
+    except Cart.DoesNotExist:
+        # If the attempt to get an existing cart with the given `user_session` doesn't exist
+        # then the user must be adding items to the cart for the first time so create a new one
+        cart = Cart(date=datetime.now(tz=timezone.utc), user_session=user_session)
+        cart.save()
+
+    return cart
