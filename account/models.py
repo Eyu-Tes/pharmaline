e325a9206cfcd6f5216a635e098c7e34f0aa0a4d
Eyu-Tes <eyoabtesfaye10@gmail.com>
2020-09-09 12:33:23 +0300

    added a PharmaAdmin model

--- account/models.py
+++ account/models.py
@@ -35,3 +35,21 @@ class Pharmacy(models.Model):
 
     def __str__(self):
         return f'{self.pharmacy_name}'
+
+
+class PharmaAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    phone = models.CharField(max_length=20, verbose_name='phone number')
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'account_admin'
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
